c87214fbcc9c1828d1d64943ccaeef37a5dfb539
HOCC2011 <hocc.2011.8.1@gmail.com>
2025-12-10 20:38:42 +0800

    Initial Commit

--- app/src/main/java/com/homecoming/carbonless/MainActivity.java
+++ app/src/main/java/com/homecoming/carbonless/MainActivity.java
@@ -1,24 +1,33 @@
 package com.homecoming.carbonless;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    int FootprintStatus = 0; //0= good, 1= bad, 2= very bad
+    ImageView BackgroundImage;
+    ImageView FeedBackground;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        BackgroundImage = findViewById(R.id.BackgroundImage);
+        FeedBackground = findViewById(R.id.FeedBackground);
+        if (FootprintStatus == 0) {
+            BackgroundImage.setImageResource(R.drawable.good_bg);
+            FeedBackground.setColorFilter(ContextCompat.getColor(this, R.color.light_blue), android.graphics.PorterDuff.Mode.SRC_IN);
+        }
     }
 }

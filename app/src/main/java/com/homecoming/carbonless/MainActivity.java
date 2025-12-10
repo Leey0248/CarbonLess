da3007f50db5830a665cb416a12e1f537fb007c6
HOCC2011 <hocc.2011.8.1@gmail.com>
2025-12-11 03:54:54 +0800

    Initial Commit

--- app/src/main/java/com/homecoming/carbonless/MainActivity.java
+++ app/src/main/java/com/homecoming/carbonless/MainActivity.java
@@ -3,6 +3,8 @@ package com.homecoming.carbonless;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,6 +19,10 @@ public class MainActivity extends AppCompatActivity {
     int FootprintStatus = 0; //0= good, 1= bad, 2= very bad
     ImageView BackgroundImage;
     ImageView FeedBackground;
+    LinearLayout NavigationRow;
+    LinearLayout HomeItem;
+    ImageView HomeButton;
+    TextView HomeText;
     ConstraintLayout main;
 
     @SuppressLint("MissingInflatedId")
@@ -27,10 +33,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         BackgroundImage = findViewById(R.id.BackgroundImage);
         FeedBackground = findViewById(R.id.FeedBackground);
+        NavigationRow = findViewById(R.id.NavigationRow);
+        HomeItem = findViewById(R.id.HomeItem);
+        HomeButton = findViewById(R.id.HomeButton);
+        HomeText = findViewById(R.id.HomeText);
         main = findViewById(R.id.main);
-        if (FootprintStatus == 0) {
+        if (FootprintStatus == 0) { // Good
             BackgroundImage.setImageResource(R.drawable.good_bg);
             FeedBackground.setColorFilter(ContextCompat.getColor(this, R.color.light_blue), android.graphics.PorterDuff.Mode.SRC_IN);
+            NavigationRow.setBackgroundColor(getResources().getColor(R.color.light_blue));
+            //Set nav row item color
+            HomeItem.getBackground().setColorFilter(ContextCompat.getColor(this, R.color.good_item_bg_color), android.graphics.PorterDuff.Mode.SRC_ATOP);
+            HomeButton.setColorFilter(ContextCompat.getColor(this, R.color.good_fg_color), android.graphics.PorterDuff.Mode.SRC_IN);
+            HomeText.setTextColor(getResources().getColor(R.color.good_fg_color));
+            // Ui bg color (only for ui errors)
             main.setBackgroundColor(getResources().getColor(R.color.light_blue));
         }
     }
